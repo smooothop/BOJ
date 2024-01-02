3070d94310ac78db64a42a73300d9409c2c63389
smoooth(o_p <152431015+smooothop@users.noreply.github.com>
2024-01-02 18:01:39 +0000

    1009풀이

--- BOJ-C/1009.c
+++ BOJ-C/1009.c
@@ -2,9 +2,17 @@
 
 int main(){
 
-    
-
-
+    int t;
+    long long a, b, sum = 0;
+    scanf("%d", &t);
+    for(int i = 0; i < t; i++){
+        scanf("%lld %lld", &a, &b);
+        for(int j = 0; j < b; j++){
+            a *= a;
+        }
+        sum = a%10;
+        printf("%lld\n", sum);
+    }
 
     return 0;
 }
